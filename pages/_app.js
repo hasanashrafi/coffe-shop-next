7a898ce946c3c75efac6859b9b7fc9ccfcf1bb5d
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-20 17:36:50 +0330

    edit Head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+
+      <Head>
+      <title>Coffee-Shop</title>
+        <link rel="icon" href="/favicon.png"  />
+      </Head>
+      <Layout>
+      <Component {...pageProps} />;
+      </Layout>
+    </>
+  )
 }
