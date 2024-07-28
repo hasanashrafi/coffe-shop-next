9d8c64f096cd61dd8cd61b833169802c8ddda26a
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-28 22:42:25 +0330

    edit border radius configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,9 +22,9 @@ module.exports = {
       "dark": " 0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
     },
 
-    borderRadius: {
-      "4xl": "2rem",
-    },
+    // borderRadius: {
+    //   "4xl": "2rem",
+    // },
     fontFamily: {
       "Dana": "Dana",
       "DanaMedium": "Dana Medium",
