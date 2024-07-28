6992b0a3cec2ab0fb876eb7bef2e7b5c9188a5ce
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-28 21:06:06 +0330

    added app svg files

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -3,64 +3,82 @@ import Link from "next/link"
 
 function Header() {
     return (
-        <div className='flex justify-around p-2 mx-auto w-[90%] border border-slate-600 rounded-4xl my-2 font-Dana'>
-            <nav className="flex gap-4 p-2 bg-red-500">
-                <div className="">
-                    <img src="./images/app-logo.png" alt="app-logo" />
+        <>
+            <svg className="hidden">
+                <symbol id="shopping-cart" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </symbol>
+                <symbol id="night-mode" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                </symbol>
+                <symbol id="sun" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                </symbol>
+                <symbol id="login" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                </symbol>
+
+
+            </svg>
+            <div className='flex justify-around items-center p-2 mx-auto bg-slate-300 backdrop-blur-md w-[95%] border border-slate-600 rounded-4xl my-2 font-Dana'>
+                <nav className="flex gap-4 p-2  w-[65%] ">
+                    <div className="">
+                        <img src="./images/app-logo.png" alt="app-logo" className="w-[45px] h-[45px] " />
+                    </div>
+
+                    <ul className="w-full flex items-center justify-between">
+                        <li>
+                            <a href="">صفحه اصلی</a>
+                        </li>
+                        <li className='flex flex-col group relative cursor-pointer py-2'>
+                            <a>فروشگاه</a>
+                            <ul className='invisible  absolute z-50  w-56 bg-gray-100 py-2 mt-6 rounded-4xl px-4 text-gray-800 shadow-xl group-hover:visible '>
+                                <li className=' '><a>قهوه ویژه</a></li>
+                                <li className=' '><a>قهوه ویژه در سطح جهانی</a></li>
+                                <li className=' '><a>قهوه درجه یک</a></li>
+                                <li className=' '><a>ترکیبات تجاری</a></li>
+                                <li className=' '><a>کپسول قهوه</a></li>
+                                <li className=' '><a>قهوه زینو برزیلی</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="">دیکشنری </a>
+                        </li>
+                        <li>
+                            <a href="">بلاگ </a>
+                        </li>
+                        <li>
+                            <a href="">درباره ما </a>
+                        </li>
+                        <li>
+                            <a href="">تماس با ما </a>
+                        </li>
+                    </ul>
+                </nav>
+                {/* card icon */}
+                <div className="flex w-[30%] justify-around text-brown-600  items-center  p-5">
+                    <div className="flex divide-x-2  gap-1 items-center ml-1">
+                        <div className="m-1">
+                            <svg className="w-[25px] h-[25px] ">
+                                <use xlinkHref="#night-mode"></use>
+                            </svg>
+                        </div>
+                        <div className="p-1 text-sm ">
+                            <svg className="w-[25px] h-[25px] ">
+                                <use xlinkHref="#shopping-cart"></use>
+                            </svg>
+                        </div>
+                    </div>
+                    <Link href="/signin" className="flex items-center mr-1">
+                        <svg className="w-[25px] h-[25px] rotate-180 ">
+                            <use xlinkHref="#login"></use>
+                        </svg>
+                        ثبت نام | ورود
+                    </Link>
                 </div>
 
-                <ul className="w-full flex items-center justify-between">
-                    <li>
-                        <a href="">صفحه اصلی</a>
-                    </li>
-                    <li className='flex flex-col group relative cursor-pointer py-2'>
-                        <a>فروشگاه</a>
-                        <ul className='invisible  absolute z-50  w-56 bg-gray-100 py-2 mt-6 rounded-4xl px-4 text-gray-800 shadow-xl group-hover:visible '>
-                            <li className=' '><a>قهوه ویژه</a></li>
-                            <li className=' '><a>قهوه ویژه در سطح جهانی</a></li>
-                            <li className=''><a>قهوه درجه یک</a></li>
-                            <li className=' '><a>ترکیبات تجاری</a></li>
-                            <li className=' '><a>کپسول قهوه</a></li>
-                            <li className=' '><a>قهوه زینو برزیلی</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="">دیکشنری </a>
-                    </li>
-                    <li>
-                        <a href="">بلاگ </a>
-                    </li>
-                    <li>
-                        <a href="">درباره ما </a>
-                    </li>
-                    <li>
-                        <a href="">تماس با ما </a>
-                    </li>
-                </ul>
-            </nav>
-            {/* card icon */}
-            <div className="flex w-[25%] justify-around  items-center  bg-blue-500 ">
-                <div className="flex divide-x-2">
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                        </svg>
-                    </div>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                        </svg>
-                    </div>
-                </div>
-                <Link href="/signin" className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
-                    </svg>
-                    ثبت نام | ورود
-                </Link>
             </div>
-
-        </div>
+        </>
     )
 }
 
