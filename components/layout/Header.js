f6f9955802bf2311cd51de3184b9d2e47770a2b6
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-28 19:24:15 +0330

    clean white space

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -38,7 +38,6 @@ function Header() {
                     </li>
                 </ul>
             </nav>
-
             {/* card icon */}
             <div className="flex w-[25%] justify-around  items-center  bg-blue-500 ">
                 <div className="flex divide-x-2">
@@ -53,7 +52,6 @@ function Header() {
                         </svg>
                     </div>
                 </div>
-
                 <Link href="/signin" className="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
