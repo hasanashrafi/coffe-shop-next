1ff8e247cee18b1db37e26a658c5fac16341b72d
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-22 09:42:17 +0330

    edit tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -14,25 +15,25 @@ module.exports = {
           "600": "#967259",
           "900": "#634832"
         }
+      },
+
+      boxShadow: {
+        "light": "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+        "dark": " 0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+      },
+
+      // borderRadius: {
+      //   "4xl": "2rem",
+      // },
+      fontFamily: {
+        "Dana": "Dana",
+        "DanaMedium": "Dana Medium",
+        "DanaDemiBold": "Dana DemiBold",
+        "MorabbaLight": "Morabba light",
+        "MorabbaMedium": "Morabba Medium",
+        "MorabbaBold": "Morabba Bold",
       }
     },
-
-    boxShadow: {
-      "light": "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
-      "dark": " 0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
-    },
-
-    // borderRadius: {
-    //   "4xl": "2rem",
-    // },
-    fontFamily: {
-      "Dana": "Dana",
-      "DanaMedium": "Dana Medium",
-      "DanaDemiBold": "Dana DemiBold",
-      "MorabbaLight": "Morabba light",
-      "MorabbaMedium": "Morabba Medium",
-      "MorabbaBold": "Morabba Bold",
-    }
   },
   plugins: [],
 };
