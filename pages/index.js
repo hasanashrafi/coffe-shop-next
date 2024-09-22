cc44e8e5aa1f6180382b53428120a5f4287194f0
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-22 11:48:07 +0330

    edit text

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-        <div className= "min-h-screen font-DanaDemiBold p-5 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          Coffee Shop
+        <div className= "min-h-screen font-DanaDemiBold p-5 max-w-5xl w-full items-center justify-between  text-sm lg:flex">
+          کافی شاپ
         </div> 
   );
 }
