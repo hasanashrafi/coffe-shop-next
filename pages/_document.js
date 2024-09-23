30a8758825d5351f9996d79f572934e46cb19cb0
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-23 11:09:03 +0330

    edit style

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="fa" dir="rtl" className="">
+    <Html lang="fa" dir="rtl" className="dark">
       <Head />
-      <body className="bg-gray-100 dark:bg-zinc-800">
+      <body className="bg-gray-100 dark:bg-zinc-800 p-10">
         <Main />
         <NextScript />
       </body>
