24b5d6a96609e9a629239f36a4f67fb50f509a56
Hasan Ashrafi <yasashrafi@gmail.com>
2024-10-06 09:15:02 +0330

    change class

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="fa" dir="rtl" className="dark">
+    <Html lang="fa" dir="rtl" className="">
       <Head />
       <body className="bg-gray-100 dark:bg-zinc-800 p-10">
         <Main />
