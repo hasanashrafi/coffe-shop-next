b5ae859f39cd7e4b39db373080d7906c713f7f03
Hasan Ashrafi <yasashrafi@gmail.com>
2024-10-06 09:14:46 +0330

    wrap theme provider around app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
@@ -10,9 +11,12 @@ export default function App({ Component, pageProps }) {
         <title>Coffee-Shop</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <ThemeProvider attribute="class">
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      </ThemeProvider>
     </>
   )
 }
