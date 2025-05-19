c83c9c9b41f77e07d6faf8499fe041d3f8e489b9
Hasan Ashrafi <yasashrafi@gmail.com>
2025-05-19 17:31:01 +0330

    edit styles

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@
 
 export default function Home() {
   return (
-    <div className=" min-h-screen font-DanaDemiBold p-5 mx-auto my-5 w-[95%] items-center justify-between  text-sm ">
-      <div>
+    <div className="  min-h-screen font-DanaDemiBold p-5 mx-auto my-5 w-[95%] items-center justify-between  text-sm ">
+    
+      <div className="">
         <p className="text-white">
           کافی شاپ
         </p>
       </div>
+
     </div>
   );
 }
