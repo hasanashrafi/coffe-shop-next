d23c13221e13d930144804d82880987ae9fd0eb5
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-01 17:47:45 +0330

    Add Products component to fetch and display product data, enhancing the product listing functionality for the Coffee-Shop application.

---
+++ src/pages/products/index.js
@@ -0,0 +1,46 @@
+import Loader from '@/components/modules/Loader'
+import ProductsPage from '@/components/ProductsPage'
+import React, { useState, useEffect } from 'react'
+
+function Products() {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch products')
+        }
+
+        const data = await response.json()
+        setData(data)
+
+      } catch (error) {
+        setError(error)
+        console.error('Error fetching products:', error)
+
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchProducts()
+  }, [])
+
+  if (loading) return <Loader />
+  if (error) return <div>Error: {error.message}</div>
+
+  return (
+    <ProductsPage data={data} />
+  )
+}
+
+export default Products
