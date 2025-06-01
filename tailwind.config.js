52c1832284dfdf6f9fb932d80dcc012441796133
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-01 13:51:40 +0330

    Refactor tailwind.config.js to consolidate content paths under src directory and integrate tailwind-scrollbar plugin for enhanced styling capabilities.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,8 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -47,6 +44,7 @@ module.exports = {
     function ({ addVariant }) {
       addVariant('child', '& > *');
       addVariant('child-hover', '& > *:hover');
-    }
+    },
+    require('tailwind-scrollbar')({ nocompatible: true }),
   ],
 };
