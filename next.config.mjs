54927bbdf2be80c3e27644f97e2f2a352faae5f4
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-01 13:51:09 +0330

    Add environment variable for API base URL in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL:'https://backend-coffeshop-next.onrender.com/api/products '
+  },
 };
 
 export default nextConfig;
