8a0aa442140ad745c816255273cb7a219c68c9fe
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-01 17:47:22 +0330

    Add Home component to serve as the main landing page for the Coffee-Shop application, featuring a welcoming message in Persian.

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div className="  min-h-screen font-DanaDemiBold p-5 mx-auto my-5  items-center justify-between  text-sm ">
+    
+      <div className="">
+        <p className="text-white">
+          کافی شاپ
+        </p>
+      </div>
+
+    </div>
+  );
+}
