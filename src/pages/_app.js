77c72de7de30492176d539f1fbe44f0f05ed7386
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-02 21:54:05 +0330

    Remove Redux Provider from App component, simplifying the structure and preparing for potential future state management adjustments in the Coffee-Shop application.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,13 @@ import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
-import { Provider } from 'react-redux';
+
 import { store } from '@/store/store';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+ <>
+
       <Head>
         <title>Coffee-Shop</title>
         <link rel="icon" href="/favicon.png" />
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }) {
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
-    </Provider>
+   
+ </>
   )
 }
