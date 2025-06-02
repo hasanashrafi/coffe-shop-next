4bfa3cb62854f59bb49b9d888cae43f1efa275a4
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-02 19:42:33 +0330

    Integrate Redux store into the App component by wrapping it with the Provider, enhancing state management capabilities for the Coffee-Shop application.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,21 +2,21 @@ import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
+import { Provider } from 'react-redux';
+import { store } from '@/store/store';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-
+    <Provider store={store}>
       <Head>
         <title>Coffee-Shop</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <ThemeProvider attribute="class">
-
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
-    </>
+    </Provider>
   )
 }
