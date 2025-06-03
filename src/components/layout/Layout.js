82e0185f2972bff44631527759017ed00d10de73
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-03 16:27:02 +0330

    Add dropdown menu functionality to mobile menu in Layout component, enhancing navigation options and user experience in the Coffee-Shop application.

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -12,6 +12,7 @@ function Layout({ children }) {
     const { systemTheme, theme, setTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
     const [mobileMenu, setMobileMenu] = useState(false);
+    const [dropDownMenu, setDropDownMenu] = useState(false)
 
     // Handle theme persistence and hydration
     useEffect(() => {
@@ -86,6 +87,27 @@ function Layout({ children }) {
                 <symbol id='home' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
                     <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </symbol>
+                <symbol id='home' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </symbol>
+                <symbol id='chat' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                </symbol>
+                <symbol id='briefcase' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                </symbol>
+                <symbol id='document' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                </symbol>
+                <symbol id='phone' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                </symbol>
+                <symbol id='chevron-down' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </symbol>
+                <symbol id='shop-bag' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                </symbol>
 
 
             </svg>
@@ -158,8 +180,8 @@ function Layout({ children }) {
                 {/* Mobile Menu Overlay */}
                 {!mobileMenu && (
                     <>
-
                         <div className='font-Dana fixed top-0 bottom-0 right-0 w-64 pt-3 px-4 min-h-screen bg-white dark:bg-zinc-700 shadow-lg z-20 scroll-smooth overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
+                            {/* nav mobile menu */}
                             <div className='flex justify-between items-center p-2 pb-5 mb-6 border-b border-b-gray-100 dark:border-b-white/10'>
                                 <div className='flex items-center justify-center  gap-x-3.5 '>
                                     <svg className='size-10 text-orange-300'>
@@ -173,12 +195,88 @@ function Layout({ children }) {
                                     <use href='#close'></use>
                                 </svg>
                             </div>
-                            <div className='flex flex-col p-4 gap-y-4 font-Dana'>
-                                <Link href="/" className="inline-flex text-zinc-700 dark:text-white hover:text-orange-300">صفحه اصلی</Link>
-                                <Link href="/menu" className="inline-flex text-zinc-700 dark:text-white hover:text-orange-300">منو</Link>
-                                <Link href="/about" className="inline-flex text-zinc-700 dark:text-white hover:text-orange-300">درباره ما</Link>
-                                <Link href="/contact" className="inline-flex text-zinc-700 dark:text-white hover:text-orange-300">تماس با ما</Link>
-                            </div>
+
+
+                            <ul className='flex flex-col p-4 gap-y-6 child:pr-2 child:p-2 text-zinc-600 dark:text-white'>
+                                <li className='bg-orange-200/20 rounded-md pr-0 h-10'>
+                                    <Link href="/" className="flex items-center  gap-x-2 text-orange-400 dark:text-orange-300 hover:text-orange-300">
+                                        <svg className='size-5'>
+                                            <use href='#home'></use>
+                                        </svg>
+                                        صفحه اصلی
+                                    </Link>
+                                </li>
+                                <li className='flex items-center pr-0 mt-4'>
+                                    <Link href="/" className="w-full flex items-center justify-between gap-x-2 text-orange-300 dark:text-white hover:text-orange-300">
+                                        <span className='flex items-center gap-x-2'>
+                                            <svg className='size-5'>
+                                                <use href='#shop-bag'></use>
+                                            </svg>
+                                            فروشگاه
+                                        </span>
+                                        <span>
+                                            <svg className={`size-5 ${dropDownMenu && "rotate-180"} transition-all ease-in-out duration-300`} onClick={() => setDropDownMenu(!dropDownMenu)}>
+                                                <use href='#chevron-down'></use>
+                                            </svg>
+                                        </span>
+                                    </Link>
+                                </li>
+                                {dropDownMenu && (
+                                    <ul className='flex flex-col justify-start list-outside child-hover:list-disc child-hover:text-orange-300  child:mr-4 transition-all ease-in-out duration-500 gap-y-4'>
+                                        <li>
+                                            <Link href="/" className='inline-flex '>قهوه ویژه</Link>
+                                        </li>
+                                        <li>
+                                            <Link href="/" className='inline-flex  '>قهوه ویژه در سطح جهانی</Link>
+                                        </li>
+                                        <li>
+                                            <Link href="/" className='inline-flex  '>قهوه درجه یک</Link>
+                                        </li>
+                                        <li>
+                                            <Link href="/" className='inline-flex  '>ترکیبات تجاری </Link>
+                                        </li>
+                                        <li>
+                                            <Link href="/" className='inline-flex  '>کپسول قهوه</Link>
+                                        </li>
+                                        <li>
+                                            <Link href="/" className='inline-flex  '>قهوه زینو برزیلی</Link>
+                                        </li>
+                                    </ul>
+                                )}
+                                <li>
+                                    <Link href="/" className="inline-flex items-center gap-x-2 text-zinc-600 dark:text-white hover:text-orange-300">
+                                        <svg className='size-5'>
+                                            <use href='#chat'></use>
+                                        </svg>
+                                        دیکشنری
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/blog" className="inline-flex  gap-x-2 text-zinc-700 dark:text-white hover:text-orange-300">
+                                        <svg className='size-5'>
+                                            <use href='#document'></use>
+                                        </svg>
+                                        بلاگ
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/about" className="inline-flex gap-x-2 text-zinc-700 dark:text-white hover:text-orange-300">
+                                        <svg className='size-5'>
+                                            <use href='#briefcase'></use>
+                                        </svg>
+                                        درباره ما
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/contact" className="inline-flex gap-x-2 text-zinc-700 dark:text-white hover:text-orange-300">
+                                        <svg className='size-5'>
+                                            <use href='#phone'></use>
+                                        </svg>
+                                        تماس با ما
+                                    </Link>
+                                </li>
+                            </ul>
+
 
                             {/* footer mobile menu */}
                             <div className='space-y-6 pt-8 mt-8 px-2.5 border-t border-t-gray-100 dark:border-t-white/10'>
@@ -220,7 +318,6 @@ function Layout({ children }) {
             <div className="min-h-screen">
                 {children}
             </div>
-
             <Footer />
         </>
     )
