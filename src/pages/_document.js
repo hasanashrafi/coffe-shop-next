c32530408411f196602c06f84b9f84ce46b41f78
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-03 11:52:39 +0330

    Update Document component to set default className to 'light', enhancing styling consistency for the Coffee-Shop application.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="fa" dir="rtl" className="">
+    <Html lang="fa" dir="rtl" className="light">
       <Head />
       <body className=" mx-auto bg-[url('/images/headerBgDesktop.webp')] bg-contain  bg-top bg-no-repeat   dark:bg-zinc-800 ">
         <Main />
