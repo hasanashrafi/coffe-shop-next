a9c6a7505523ad4f329ef2c2bb4165a919c9740e
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-05 15:05:11 +0330

    Refactor NavBar component to replace anchor tags with Link components for improved navigation and consistency, enhancing user experience in the Coffee-Shop application.

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,42 +2,44 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
+
 function NavBar() {
   return (
     <nav className="flex items-center gap-x-6 lg:gap-x-9  h-14">
-    <div className="shrink-0">
+      <div className="shrink-0">
         <Image src="/images/app-logo.png" priority width={100} height={100} alt="app-logo" className="w-[50px] h-[45px] " />
-    </div>
-    <ul className="w-full h-full flex items-center text-md lg:text-md tracking-tightest gap-x-5  lg:gap-x-9 text-gray-300">
+      </div>
+      <ul className="w-full h-full flex items-center text-md lg:text-md tracking-tightest gap-x-5  lg:gap-x-9 text-gray-300">
         <li className="font-DanaMedium text-orange-200">
-        <Link href="/">صفحه اصلی</Link>
+          <Link href="/">صفحه اصلی</Link>
         </li>
         <li className='group relative '>
-        <Link
+          <Link
             href='/products'
             className=' group-hover:text-orange-300 transition-colors cursor-pointer'>فروشگاه</Link>
-            <div className=' bg-white invisible  opacity-0 mt-2 absolute z-50 delay-75  w-52 bg-whitedrop-shadow-lg p-6 rounded-2xl border-t-4 border-t-yellow-500 shadow-dark  group-hover:opacity-100 leading-[56px]  group-hover:visible tracking-normal top-full text-base transition-all ease-in-out space-y-4 dark:bg-zinc-700 dark:child:text-white dark:child-hover:text-orange-300 first-line:text-yellow-500 child:block  child:text-sm  child-hover:text-orange-300 child:transition-colors child:text-slate-900'>
-                <a href='#'>قهوه ویژه</a>
-                <a href='#'>قهوه ویژه در سطح جهانی</a>
-                <a href='#'>قهوه درجه یک</a>
-                <a href='#'>ترکیبات تجاری</a>
-                <a href='#'>کپسول قهوه</a>
-                <a href='#'>قهوه زینو برزیلی</a></div>
+          <div className=' bg-white invisible  opacity-0 mt-2 absolute z-50 delay-75  w-52 bg-whitedrop-shadow-lg p-6 rounded-2xl border-t-4 border-t-yellow-500 shadow-dark  group-hover:opacity-100 leading-[56px]  group-hover:visible tracking-normal top-full text-base transition-all ease-in-out space-y-4 dark:bg-zinc-700 dark:child:text-white dark:child-hover:text-orange-300 first-line:text-yellow-500 child:block  child:text-sm  child-hover:text-orange-300 child:transition-colors child:text-slate-900'>
+            <Link href='#'>قهوه ویژه</Link>
+            <Link href='#'>قهوه ویژه در سطح جهانی</Link>
+            <Link href='#'>قهوه درجه یک</Link>
+            <Link href='#'>ترکیبات تجاری</Link>
+            <Link href='#'>کپسول قهوه</Link>
+            <Link href='#'>قهوه زینو برزیلی</Link>
+          </div>
         </li>
         <li>
-        <a href="">دیکشنری </a>
+          <Link href="">دیکشنری </Link>
         </li>
         <li>
-        <a href="">بلاگ </a>
+          <Link href="">بلاگ </Link>
         </li>
         <li>
-        <a href="">درباره ما </a>
+          <Link href="">درباره ما </Link>
         </li>
         <li>
-        <a href="">تماس با ما </a>
+          <Link href="">تماس با ما </Link>
         </li>
-    </ul>
-</nav>
+      </ul>
+    </nav>
   )
 }
 
