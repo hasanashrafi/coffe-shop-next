05559603bdd4a6853f165d8fa4a5ce9d8eec3f54
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-05 15:04:31 +0330

    Add DarkModeButton component to layout for theme toggling, enhancing user experience in the Coffee-Shop application.

---
+++ src/components/layout/DarkModeButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ThemeToggle from './ThemeToggle';
+
+const DarkModeButton = () => {
+    return (
+        <ThemeToggle className="" label={false} />
+    );
+};
+
+export default DarkModeButton; 
