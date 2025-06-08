c030cbd0e40ce85b0856f7cef64d55dbb4cdb301
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-08 22:34:59 +0330

    Add shopping bag button to ProductCard component for enhanced user interaction

--- src/components/templates/ProductCard.js
+++ src/components/templates/ProductCard.js
@@ -1,23 +1,24 @@
 import Image from 'next/image'
 import React from 'react'
+import { FaShoppingBag } from 'react-icons/fa';
 
 function ProductCard(product) {
     const { name, price, discount, image } = product
 
     return (
-        <div className="group flex items-center bg-white dark:bg-zinc-700 gap-x-1 md:gap-x-2.5 mt-5 pb-6  border-b border-b-gray-300 dark:border-b-white/10">
+        <div className="md:w-[350px] group flex items-center bg-white dark:bg-zinc-700 gap-x-1 md:gap-x-2.5 rounded-xl mt-5 p-5 md:p-3  ">
             {/* Product Image */}
             <Image
                 priority
                 src={image}
-                className="w-[90px] h-[90px] md:w-[120px] md:h-[120px]  object-cover transform hover:scale-110 transition-transform duration-500"
+                className=" w-[90px] h-[90px] md:w-[120px] md:h-[120px]  object-cover transform hover:scale-110 transition-transform duration-500"
                 width={120}
                 height={120}
                 alt={`${name}`}
             />
 
             {/* Product Info */}
-            <div className="flex flex-col justify-between gap-y-1.5">
+            <div className=" flex flex-col justify-between  gap-y-1.5">
                 <h4 className="font-DanaMedium dark:text-white text-zinc-700 text-sm md:text-base line-clamp-2 ">
                     {name}
                 </h4>
@@ -26,7 +27,7 @@ function ProductCard(product) {
                     {/* discount */}
                     <div className="">
                         {discount > 0 && (
-                            <p className="text-teal-600 dark:text-emerald-500 rounded-lg text-xs font-DanaMedium">
+                            <p className="text-teal-600 dark:text-emerald-500  text-xs font-DanaMedium">
                                 {discount}% تخفیف
                             </p>
                         )}
@@ -45,6 +46,9 @@ function ProductCard(product) {
                             {discount > 0 ? Math.round(price * (1 - discount / 100)) : price}
                             <span className="font-Dana text-xs md:text-sm mr-1">تومان</span>
                         </div>
+                        <button className="hidden md:inline-block bg-teal-600 dark:bg-teal-500 text-white dark:text-white px-2 py-1 rounded-lg text-lg md:text-sm  transition-colors hover:bg-teal-700 dark:hover:bg-teal-600">
+                             <FaShoppingBag className='text-xl '/>  
+                        </button>
                     </div>
                 </div>
 
