0214259748c83efaee8a5361a6caa192a15f2a96
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-08 22:35:09 +0330

    Add sampleProducts data for product listings

---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+const sampleProducts = [
+    {
+        id: 1,
+        name: "قهوه اسپرسو بن مانو مدل پرسکا 250 گرمی",
+        price: 150000,
+        discount: 10,
+        image: "/images/products/p1.png"
+    },
+    {
+        id: 4,
+        name: "قهوه اسپرسو بن مانو مدل پرسکا 250 گرمی",
+        price: 150000,
+        discount: 10,
+        image: "/images/products/p1.png"
+    },
+    {
+        id: 2,
+        name: "قهوه اسپرسو بن مانو مدل پرسکا 250 گرمی",
+        price: 150000,
+        discount: 10,
+        image: "/images/products/p1.png"
+    },
+    {
+        id: 3,
+        name: "قهوه اسپرسو بن مانو مدل پرسکا 250 گرمی",
+        price: 150000,
+        discount: 10,
+        image: "/images/products/p1.png"
+    },
+];
+
+export default sampleProducts;
