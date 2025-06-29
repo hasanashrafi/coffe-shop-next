ddee6e6eac9f707a088c9862241eac0b36f38cf9
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-29 19:17:15 +0330

    create main and responsive it

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,16 @@
 
 export default function Home() {
   return (
-    <div className="  min-h-screen font-DanaDemiBold p-5 mx-auto my-5  items-center justify-between  text-sm ">
-    
-      <div className="">
-        <p className="text-white">
-          کافی شاپ
-        </p>
-      </div>
-
-    </div>
+    <main className="container  min-h-screen font-DanaDemiBold ">
+      <section className="home">
+        <div className="container">
+          <h2 className="md:text-6xl font-bold  font-MorabbaBold text-white">
+            قهوه عربیکا تانزانیا
+          </h2>
+          <span className="font-MorabbaLight md:text-5xl text-white">یک فنجان بالانس</span>
+          <span className="block w-[100px] h-0.5 bg-orange-300"></span>
+        </div>
+      </section>
+    </main>
   );
 }
