43c300e86fd150059ea6a2c5baa601c4715b09eb
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-30 19:02:31 +0330

    `Refactor index page layout and styles`

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -103,6 +103,9 @@ function Layout({ children }) {
                 <symbol id='shop-bag' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                 </symbol>
+                <symbol id='curve' width="100" height="22" viewBox="0 0 100 22" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M50 0C69 0 81 22 100 22L0 22C18.75 22 31 0 50 0Z"  />
+                </symbol>
 
 
             </svg>
@@ -114,7 +117,7 @@ function Layout({ children }) {
                 currentTheme={currentTheme}
                 darkMode={darkMode}
             />
-            <div className="min-h-screen">
+            <div className="">
                 {children}
             </div>
             <Footer />
