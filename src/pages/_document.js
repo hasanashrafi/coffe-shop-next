a456977e67759b2923a5b97d62a6ef8fd0cb4894
Hasan Ashrafi <yasashrafi@gmail.com>
2025-06-30 19:04:16 +0330

    edit styles and bg

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="fa" dir="rtl" className="light">
       <Head />
-      <body className=" mx-auto bg-[url('/images/headerBgDesktop.webp')] bg-contain  bg-top bg-no-repeat   dark:bg-zinc-800 ">
+      <body className=" dark:bg-zinc-800 ">
         <Main />
         <NextScript />
       </body>
