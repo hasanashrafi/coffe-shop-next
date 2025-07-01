93635d41de385a75ab948f122d04dc1e381658ae
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-01 12:33:14 +0330

    added chevron left

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -104,8 +104,12 @@ function Layout({ children }) {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                 </symbol>
                 <symbol id='curve' width="100" height="22" viewBox="0 0 100 22" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M50 0C69 0 81 22 100 22L0 22C18.75 22 31 0 50 0Z"  />
+                    <path d="M50 0C69 0 81 22 100 22L0 22C18.75 22 31 0 50 0Z" />
                 </symbol>
+                <symbol id='chevron-left' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                    <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                </symbol>
+
 
 
             </svg>
