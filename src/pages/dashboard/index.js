b799ddb55f4eb5c0199875f20a4b5ff364f9bd46
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-09 22:29:39 +0330

    Add Dashboard component to render DashboardPage for user profile display

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,13 @@
+
+
+import DashboardPage from '@/components/DashboardPage'
+import React from 'react'
+
+function Dashboard() {
+
+  return (
+   <DashboardPage/>
+  )
+}
+
+export default Dashboard
