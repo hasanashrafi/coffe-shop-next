f480626511d858dcfe6b4a61662c8b3e403c46cf
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-09 22:28:23 +0330

    Refactor NavBar component to update dropdown styles, including border and background adjustments for improved visibility

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -18,7 +18,7 @@ function NavBar() {
           <Link
             href='/products'
             className=' group-hover:text-orange-300 transition-colors cursor-pointer'>فروشگاه</Link>
-          <ul className=' bg-white invisible  opacity-0 mt-2 absolute z-50 delay-75  w-52 bg-whitedrop-shadow-lg p-6 rounded-2xl border-t-4 border-t-yellow-500 shadow-dark  group-hover:opacity-100 leading-[56px]  group-hover:visible tracking-normal top-full text-base transition-all ease-in-out space-y-4 dark:bg-zinc-700 dark:child:text-white dark:child-hover:text-orange-300 first-line:text-yellow-500 child:block  child:text-sm  child-hover:text-orange-300 child:transition-colors child:text-slate-900'>
+          <ul className='  invisible  opacity-0 mt-2 absolute z-50 delay-75  w-52 bg-white drop-shadow-lg p-6 rounded-2xl border-t-2 border-t-orange-200 shadow-dark  group-hover:opacity-100 leading-[56px]  group-hover:visible tracking-normal top-full text-base transition-all ease-in-out space-y-4 dark:bg-zinc-700 dark:child:text-white dark:child-hover:text-orange-300 first-line:text-orange-300 child:block  child:text-sm  child-hover:text-orange-300 child:transition-colors child:text-slate-900'>
             <Links />
           </ul>
         </li>
