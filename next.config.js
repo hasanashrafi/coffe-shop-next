9b64b28d8846d96c79f8ab0ddc46d92fda5f0909
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-09 22:25:16 +0330

    Update API rewrites in next.config.js to point to production backend

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: '/api/:path*',
-                destination: 'http://localhost:10000/api/:path*' // Development
+                source: '/api/products',
+                destination: 'https://backend-coffeshop.onrender.com/api/products'
             }
         ]
     }
