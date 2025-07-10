3dfe9d4247d54ba6b862fdca63d4e88f6484411a
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-10 12:35:18 +0330

    Add user SVG icon to Layout component and adjust div class for children rendering

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -110,6 +110,9 @@ function Layout({ children }) {
                     <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
                 </symbol>
 
+                <symbol id='user' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                    <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+                </symbol>
 
 
             </svg>
@@ -121,7 +124,7 @@ function Layout({ children }) {
                 currentTheme={currentTheme}
                 darkMode={darkMode}
             />
-            <div className="min-h-screen">
+            <div className="">
                 {children}
             </div>
             <Footer />
