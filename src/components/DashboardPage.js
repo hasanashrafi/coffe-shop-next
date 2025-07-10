878b249276864246d5de583c6c3e77448138e53e
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-10 12:34:53 +0330

    Enhance DashboardPage with token validation and logout functionality, redirecting unauthenticated users to the signin page

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,17 +1,29 @@
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
 
 function DashboardPage() {
     const [userDetail, setUserDetail] = useState([])
+    const router = useRouter()
+
     useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            router.push('/signin');
+            return;
+        }
         try {
-            const token = localStorage.getItem('token');
             fetch("http://localhost:3004/api/users/profile", {
                 headers: {
-                    'Authorization': token ? `Bearer ${token}` : ''
+                    'Authorization': `Bearer ${token}`
                 }
             })
                 .then(res => res.json())
                 .then(data => {
+                    if (data.message === 'Invalid token' || data.message === 'Authentication required') {
+                        localStorage.removeItem('token');
+                        router.push('/signin');
+                        return;
+                    }
                     setUserDetail(data.data)
                     console.log(data)
                 })
@@ -22,11 +34,22 @@ function DashboardPage() {
             console.log(error.message)
         }
     }, [])
+
+    const logoutHandler = () => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("user")
+        router.push("/signin")
+    }
     return (
-        <div>
-            <p>dashboard</p>
-            <p>{userDetail.username}</p>
-            <p>{userDetail.email}</p>
+        <div className='font-DanaMedium pt-8 md:pt-24 lg:pt-48  min-h-screen '>
+            <div className='container '>
+                <div className='flex items-center justify-between '>
+                    <div><p className='text-2xl md:text-4xl font-DanaDemiBold'>به گلدن کافی خوش آمدید!☕</p>
+                        <p className='p-2 m-2'>نام کاربری: {userDetail?.username}</p>
+                        <p className='p-2 m-2'>ایمیل: {userDetail?.email}</p></div>
+                    <button onClick={logoutHandler} className='py-2 px-4 rounded-md text-white bg-red-600'>خروج</button>
+                </div>
+            </div>
         </div>
     )
 }
