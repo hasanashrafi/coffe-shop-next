982a19628acd95d3c8f5a4c71311d575d098711e
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-12 10:51:13 +0330

    Add request interceptor to API for automatic token inclusion in headers

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,4 +11,12 @@ export const loginUser = async (data) => {
     return await api.post(`users/signin`, data)
 }
 
+api.interceptors.request.use(async (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
 export default api;
