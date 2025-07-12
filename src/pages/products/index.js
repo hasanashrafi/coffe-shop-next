3cc704f8ba057241abff7b08145dbe642c6bf04c
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-12 21:09:30 +0330

    Refactor Products component to utilize custom hook for product data fetching, removing local state management and enhancing code clarity.

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,36 +1,12 @@
 import Loader from '@/components/modules/Loader'
 import ProductsPage from '@/components/ProductsPage'
-import React, { useState, useEffect } from 'react'
+import { useProducts } from '@/hooks/useProducts'
 
 function Products() {
-  const [data, setData] = useState([])
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch('http://localhost:3004/api/products')
-        if (!response.ok) throw new Error('Failed to fetch products')
-        const data = await response.json()
-        console.log(data)
-        setData(data || [])
-      } catch (error) {
-        setError(error)
-        console.error('Error fetching products:', error)
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchProducts()
-  }, [])
-
-  if (loading) return <Loader />
-  if (error) return <div className='h-screen flex items-center justify-center bg-red-400 text-3xl'>Error: {error.message}</div>
-
+  
   return (
-    <ProductsPage data={data} />
+    <ProductsPage />
   )
 }
 
-export default Products
+export default Products   
