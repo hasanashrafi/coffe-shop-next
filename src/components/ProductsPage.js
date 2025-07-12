f668bedc8c65af46af1a0572b6fa573050b3d644
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-12 21:04:26 +0330

    Refactor ProductsPage component to utilize custom hook for product data fetching, implement loading and error states, and enhance layout with new styling and navigation link.

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,17 +1,49 @@
 import React from 'react'
-import ProductCard from './templates/ProductCard'
+import ProductCardMain from './templates/ProductCardMain'
+import Link from 'next/link'
+import Loader from './modules/Loader';
+import { useProducts } from '@/hooks/useProducts';
 
-function ProductsPage({ data }) {
-console.log(data)
+function ProductsPage() {
+  const { products, loading, error } = useProducts();
+  console.log(products)
+  
+  if (loading) return <Loader />
+  if (error) return <div className='h-screen flex items-center justify-center bg-red-400 text-3xl'>Error: {error}</div>
+
   return (
-    <>
-      {/* Products Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 child:h-[460px] child:bg-white child:dark:bg-zinc-700 child:rounded-xl ">
-        {data && data.map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
+    <section className=" font-Dana products pt-8 md:pt-32 lg:pt-48 md:bg-products-hero md:dark:bg-[url(/images/products-bg.png)]  min-h-screen ">
+      <div className="container">
+        {/* section head */}
+        <div className="flex items-end justify-between mb-5 md:mb-12">
+          <div className="font-MorabbaMedium ">
+            <h3 className="text-2xl md:text-5xl text-zinc-700 dark:text-white">جدیدترین محصولات</h3>
+            <p className="text-lg md:text-3xl font-MorabbaLight mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white">فراوری شده از دانه های قهوه</p>
+          </div>
+          <Link href="/products" className="flex items-center text-base md:text-xl md:gap-x-1 text-orange-300 transition-colors md:hover:bg-orange-300/20 h-10 pr-3 pl-1 rounded-md tracking-tightest">
+            <span className="hidden md:inline-block">
+              مشاهده همه محصولات
+            </span>
+            <span className="md:hidden">
+              مشاهده همه
+            </span>
+            <svg className="size-5 ">
+              <use href="#chevron-left"></use>
+            </svg>
+          </Link>
+        </div>
+
+        {/* section body */}
+
+
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4  gap-3.5 md:gap-5 child:h-[200px] child:md:h-[460px] child:bg-white child:dark:bg-zinc-700 child:rounded-xl mb-20">
+          {products && products.map((product) => (
+            <ProductCardMain key={product.id} {...product} />
+          ))}
+        </div>
+
       </div>
-    </>
+    </section>
 
   )
 }
