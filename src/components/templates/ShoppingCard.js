c35e26d6d0455623be01086a4baf0906ed2a6094
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-12 21:07:25 +0330

    Update ShoppingCard component to import ProductCard from ProductCardBasket for consistency with recent refactors.

--- src/components/templates/ShoppingCard.js
+++ src/components/templates/ShoppingCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ProductCard from './ProductCard'
+import ProductCard from './ProductCardBasket'
 import Link from 'next/link';
 import sampleProducts from '@/data/data';
 
