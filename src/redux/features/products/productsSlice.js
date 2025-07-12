c82935621c092e9276adcb0441c82127b931bd8b
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-12 21:10:38 +0330

    Add productsSlice to manage product state with Redux, including async fetching and error handling.

---
+++ src/redux/features/products/productsSlice.js
@@ -0,0 +1,38 @@
+import api from "@/utils/api";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    products: [],
+    loading: false,
+    error: null,
+};
+
+const fetchProducts = createAsyncThunk("products/fetchProducts", async () => {
+    const response = await api.get("/products");
+    // Only return the data, not the entire response object
+    return response.data;
+});
+
+const productsSlice = createSlice({
+    name: "products",
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchProducts.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        builder.addCase(fetchProducts.fulfilled, (state, action) => {
+            state.products = action.payload;
+            state.loading = false;
+            state.error = null;
+        })
+        builder.addCase(fetchProducts.rejected, (state, action) => {
+            state.products = [];
+            state.loading = false;
+            state.error = action.error.message;
+        })
+    }
+})
+
+export default productsSlice.reducer;
+export { fetchProducts }
