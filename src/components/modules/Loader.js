4bd46a7653f25c8ce47c6720a2e37a2e6771e4db
Hasan Ashrafi <yasashrafi@gmail.com>
2025-07-20 23:11:47 +0330

    Remove merge conflict markers and clean up the ProductsPage, Loader, and globals.css components for improved code clarity and consistency.

--- src/components/modules/Loader.js
+++ src/components/modules/Loader.js
@@ -2,17 +2,10 @@ import React from 'react'
 
 function Loader() {
     return (
-<<<<<<< HEAD
-        <div className="font-Dana flex flex-col justify-center items-center h-screen ">
-            <div className="relative">
-                {/* Coffee Cup */}
-                <div className="w-24 h-24  rounded-b-3xl rounded-t-lg relative">
-=======
         <div className="font-Dana flex flex-col justify-center items-center h-screen">
             <div className="relative">
                 {/* Coffee Cup */}
                 <div className="w-24 h-24 rounded-b-3xl rounded-t-lg relative">
->>>>>>> ui
                     <div className="absolute top-0 left-0 w-full h-8 bg-amber-800 rounded-t-lg"></div>
                     <div className="absolute top-2 left-2 w-20 h-16 bg-amber-100 rounded-b-2xl"></div>
                 </div>
