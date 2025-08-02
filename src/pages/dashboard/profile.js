2faf19587fd1e6a9f69a74af2323ce75c67cca55
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-02 16:46:11 +0330

    Refactor authentication process by removing unused signup and signin API endpoint implementations, streamlining the codebase for improved efficiency.

---
+++ src/pages/dashboard/profile.js
@@ -0,0 +1,10 @@
+import DashboardProfile from '@/components/DashboardProfile'
+import React from 'react'
+
+function ProfilePage() {
+    return (
+        <DashboardProfile />
+    )
+}
+
+export default ProfilePage 
