bca96625d8ede4ed699d5d72a9a4e4b87c243939
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-02 16:45:18 +0330

    Refactor Footer component by removing unnecessary margin class for cleaner layout and improved styling consistency.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='mt-96 place-content-center  backdrop-blur-xl h-14 mx-auto text-center'>
+        <footer className='  place-content-center   h-14 mx-auto text-center'>
             Developed By HsN
         </footer>
     )
