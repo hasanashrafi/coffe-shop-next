5d6cd0f8b5afbafdb0f130d991f0f27305e35cba
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-09 12:41:18 +0330

    Refactor CategoryBanner component to improve background styling and text size adjustments for better visual consistency and responsiveness.

--- src/components/CategoryBanner.js
+++ src/components/CategoryBanner.js
@@ -6,19 +6,19 @@ function CategoryBanner() {
             <div className='container max-w-7xl '>
 
                <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-               <div className='flex flex-col justify-center items-start pr-12  bg-category-right rounded-2xl  h-[248px] '>
-                    <h5 className='font-DanaDemiBold text-xl md:text-4xl mb-7 text-zinc-700 dark:text-white'>
+               <div className='bg-cover flex flex-col justify-center items-start  pr-12  bg-category-right rounded-2xl  h-[142px] md:h-[248px] '>
+                    <h5 className='font-DanaDemiBold text-xl md:text-4xl/6 mb-7 text-zinc-700 dark:text-white'>
                         انواع قهوه
                     </h5>
-                    <p className='md:text-xl md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
+                    <p className='md:text-xl/6 md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
                         ترکیبی و تک خاستگاه
                     </p>
                 </div>
-                <div className='flex flex-col justify-center items-start pr-12 bg-category-left  rounded-2xl  h-[248px] '>
-                    <h5 className='font-DanaDemiBold text-xl md:text-4xl mb-7 text-zinc-700 dark:text-white'>
+                <div className='bg-cover flex flex-col justify-center items-start  pr-12 bg-category-left  rounded-2xl  h-[142px] md:h-[248px] '>
+                    <h5 className='font-DanaDemiBold text-xl/6 md:text-4xl/6 mb-7 text-zinc-700 dark:text-white'>
                         پودر های فوری
                     </h5>
-                    <p className=' md:text-xl md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
+                    <p className=' md:text-xl/6 md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
                         نسکافه ، هات چاکلت ، ماسالا
                     </p>
                 </div>
