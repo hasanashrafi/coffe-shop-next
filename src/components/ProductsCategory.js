0bf682fb73d4d08153b23e1797001e01e65faba6
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-09 17:09:29 +0330

    Add ProductsCategory component to display product categories with images and links for navigation, enhancing user experience and layout consistency.

---
+++ src/components/ProductsCategory.js
@@ -0,0 +1,72 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function ProductsCategory() {
+    return (
+        <section className='flex flex-wrap items-center justify-center gap- mb-10 md:mb-20'>
+            <Link href="/" className=' w-25 md:w-50 text-center '>
+                <Image
+                    src="/images/categories/category1.png"
+                    alt='category'
+                    width={100}
+                    height={100}
+                    loading='lazy'
+                    className='mx-auto' />
+                <span className='inline-block mt-1.5 md:mt-2.5 font-DanaDemiBold text-zinc-700 dark:text-white text-sm md:text-xl'>
+                    لوازم جانبی و تجهیزات
+                </span>
+            </Link>
+            <Link href="/" className='w-25 md:w-50 text-center '>
+                <Image
+                    src="/images/categories/category2.png"
+                    alt='category'
+                    width={100}
+                    height={100}
+                    loading='lazy'
+                    className='mx-auto' />
+                <span className='inline-block mt-1.5 md:mt-2.5 font-DanaDemiBold text-zinc-700 dark:text-white text-sm md:text-xl'>
+                    اسپرسو ساز
+                </span>
+            </Link>
+            <Link href="/" className='w-25 md:w-50 text-center '>
+                <Image
+                    src="/images/categories/category3.png"
+                    alt='category'
+                    width={100}
+                    height={100}
+                    loading='lazy'
+                    className='mx-auto' />
+                <span className='inline-block mt-1.5 md:mt-2.5 font-DanaDemiBold text-zinc-700 dark:text-white text-sm md:text-xl'>
+                    پک تستر قهوه
+                </span>
+            </Link>
+            <Link href="/" className='w-25 md:w-50 text-center '>
+                <Image
+                    src="/images/categories/category4.png"
+                    alt='category'
+                    width={100}
+                    height={100}
+                    loading='lazy'
+                    className='mx-auto' />
+                <span className='inline-block mt-1.5 md:mt-2.5 font-DanaDemiBold text-zinc-700 dark:text-white text-sm md:text-xl'>قهوه و اسپرسو</span>
+            </Link>
+            <Link href="/" className='w-25 md:w-50 text-center '>
+                <Image
+                    src="/images/categories/category5.png"
+                    alt='category'
+                    width={100}
+                    height={100}
+                    loading='lazy'
+                    className='mx-auto' />
+                <span className='inline-block mt-1.5 md:mt-2.5 font-DanaDemiBold text-zinc-700 dark:text-white text-sm md:text-xl'>
+                    قهوه ترک
+                </span>
+            </Link>
+        </section>
+    )
+}
+
+export default ProductsCategory
+
+
