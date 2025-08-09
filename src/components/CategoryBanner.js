668f18157cf6b2f940368aaf1f6ce10f045c3bd6
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-09 09:52:09 +0330

    Update CategoryBanner component styling for improved layout and responsiveness, including adjustments to text sizes and background classes for better visual hierarchy.

--- src/components/CategoryBanner.js
+++ src/components/CategoryBanner.js
@@ -6,19 +6,19 @@ function CategoryBanner() {
             <div className='container max-w-7xl '>
 
                <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-               <div className=' bg-white/20 rounded-2xl  h-[248px] '>
-                    <h5 className='text-4xl text-zinc-700 dark:text-white'>
+               <div className='flex flex-col justify-center items-start pr-12  bg-category-right rounded-2xl  h-[248px] '>
+                    <h5 className='font-DanaDemiBold text-xl md:text-4xl mb-7 text-zinc-700 dark:text-white'>
                         انواع قهوه
                     </h5>
-                    <p className='text-xl font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
+                    <p className='md:text-xl md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
                         ترکیبی و تک خاستگاه
                     </p>
                 </div>
-                <div className='bg-white/20 rounded-2xl  h-[248px] '>
-                    <h5 className='text-4xl text-zinc-700 dark:text-white'>
+                <div className='flex flex-col justify-center items-start pr-12 bg-category-left  rounded-2xl  h-[248px] '>
+                    <h5 className='font-DanaDemiBold text-xl md:text-4xl mb-7 text-zinc-700 dark:text-white'>
                         پودر های فوری
                     </h5>
-                    <p className='text-xl font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
+                    <p className=' md:text-xl md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
                         نسکافه ، هات چاکلت ، ماسالا
                     </p>
                 </div>
