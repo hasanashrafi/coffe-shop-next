89f9ca7e799b6ca5b2210f9547589568f34ae0e4
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-09 17:09:18 +0330

    Refactor CategoryBanner component to include links for navigation, enhance layout spacing, and improve text styling for better visual consistency and user experience.

--- src/components/CategoryBanner.js
+++ src/components/CategoryBanner.js
@@ -1,27 +1,29 @@
+import Link from 'next/link'
 import React from 'react'
 
 function CategoryBanner() {
     return (
-        <section className='my-20'>
+        <section className='mt-8 mb-10 md:my-20'>
             <div className='container max-w-7xl '>
-
                <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-               <div className='bg-cover flex flex-col justify-center items-start  pr-12  bg-category-right rounded-2xl  h-[142px] md:h-[248px] '>
-                    <h5 className='font-DanaDemiBold text-xl md:text-4xl/6 mb-7 text-zinc-700 dark:text-white'>
+              
+               <Link href='/products' className='bg-cover flex flex-col justify-center items-start pr-7 md:pr-12  bg-category-right rounded-2xl  h-[142px] md:h-[248px] '>
+                    <span className='font-DanaDemiBold text-xl md:text-4xl/6 mb-4 md:mb-7 text-zinc-700 dark:text-white'>
                         انواع قهوه
-                    </h5>
+                    </span>
                     <p className='md:text-xl/6 md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
                         ترکیبی و تک خاستگاه
                     </p>
-                </div>
-                <div className='bg-cover flex flex-col justify-center items-start  pr-12 bg-category-left  rounded-2xl  h-[142px] md:h-[248px] '>
-                    <h5 className='font-DanaDemiBold text-xl/6 md:text-4xl/6 mb-7 text-zinc-700 dark:text-white'>
+                </Link>
+
+                <Link href='/' className='bg-cover flex flex-col justify-center items-start pr-7 md:pr-12 bg-category-left  rounded-2xl  h-[142px] md:h-[248px] '>
+                    <span className='font-DanaDemiBold text-xl/6 md:text-4xl/6 mb-4 md:mb-7 text-zinc-700 dark:text-white'>
                         پودر های فوری
-                    </h5>
+                    </span>
                     <p className=' md:text-xl/6 md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
                         نسکافه ، هات چاکلت ، ماسالا
                     </p>
-                </div>
+                </Link>
 
                </div>
             </div>
