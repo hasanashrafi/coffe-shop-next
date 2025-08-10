0e6b089639d9306cd5ff2cb11792a124d5bf6af2
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-10 14:45:07 +0330

    Add chevron-right SVG symbol to Layout component for enhanced navigation icons

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -109,6 +109,9 @@ function Layout({ children }) {
                 <symbol id='chevron-left' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" >
                     <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
                 </symbol>
+                <symbol id='chevron-right' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" >
+                    <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                </symbol>
 
                 <symbol id='user' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" >
                     <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
