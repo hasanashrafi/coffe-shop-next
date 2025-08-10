44e4f1818823cfc56ce94e326042e6629a24fe98
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-10 14:44:51 +0330

    Update text colors in CategoryBanner component for improved readability and visual consistency across links.

--- src/components/CategoryBanner.js
+++ src/components/CategoryBanner.js
@@ -8,19 +8,19 @@ function CategoryBanner() {
                <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
               
                <Link href='/products' className='bg-cover flex flex-col justify-center items-start pr-7 md:pr-12  bg-category-right rounded-2xl  h-[142px] md:h-[248px] '>
-                    <span className='font-DanaDemiBold text-xl md:text-4xl/6 mb-4 md:mb-7 text-zinc-700 dark:text-white'>
+                    <span className='font-DanaDemiBold text-xl md:text-4xl/6 mb-4 md:mb-7  text-white'>
                         انواع قهوه
                     </span>
-                    <p className='md:text-xl/6 md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
+                    <p className='md:text-xl/6 md:font-DanaMedium mt-0.5 md:mt-1.5  text-white'>
                         ترکیبی و تک خاستگاه
                     </p>
                 </Link>
 
                 <Link href='/' className='bg-cover flex flex-col justify-center items-start pr-7 md:pr-12 bg-category-left  rounded-2xl  h-[142px] md:h-[248px] '>
-                    <span className='font-DanaDemiBold text-xl/6 md:text-4xl/6 mb-4 md:mb-7 text-zinc-700 dark:text-white'>
+                    <span className='font-DanaDemiBold text-xl/6 md:text-4xl/6 mb-4 md:mb-7  text-white'>
                         پودر های فوری
                     </span>
-                    <p className=' md:text-xl/6 md:font-DanaMedium mt-0.5 md:mt-1.5 text-zinc-700 dark:text-white'>
+                    <p className=' md:text-xl/6 md:font-DanaMedium mt-0.5 md:mt-1.5  text-white'>
                         نسکافه ، هات چاکلت ، ماسالا
                     </p>
                 </Link>
