60a551f1fc0e9a100d1775e22932ca486a5c52bd
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-11 18:08:00 +0330

    Remove sensitive MONGO_URI from environment variables in next.config.mjs for improved security and best practices.

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BASE_URL:'https://backend-coffeshop-next.onrender.com/api',
-    MONGO_URI:"mongodb+srv://hasan:102310230@cluster0.qjygrev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   },
 };
 
