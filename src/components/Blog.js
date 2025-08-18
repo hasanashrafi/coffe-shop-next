5265eade80da79884f0ec1453579b51f2f74fed1
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-18 21:31:57 +0330

    Enhance Blog component card layout with improved hover effects and updated date display for better user interaction and visual appeal.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -27,7 +27,7 @@ function Blog() {
                 </div>
                 {/* section content */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
-                    <div className='bg-white dark:bg-zinc-700 p-2.5 md:pb-2 rounded-2xl shadow-md'>
+                    <div className='group bg-white dark:bg-zinc-700 p-2.5 md:pb-2 rounded-2xl shadow-md'>
                         <div className='relative mb-4 rounded-2xl rounded-bl-4xl overflow-hidden'>
                             <Image
                                 width={100}
@@ -39,19 +39,23 @@ function Blog() {
                                 sizes='100vw'
                                 quality={100}
                             />
-                            <div className='absolute inset-0  md:flex-center hidden  bg-gradient-to-r from-orange-200/80 to-orange-300/80 rounded-xl'>
+                            <div className='absolute inset-0  md:flex-center hidden invisible cursor-pointer opacity-0 group-hover:visible group-hover:opacity-100  transition-all  delay-75 bg-gradient-to-r from-orange-200/80 to-orange-300/80 rounded-xl'>
                                 <svg className='w-[138px] h-[58px] text-amber-700'>
                                     <use href='#logo-type' />
                                 </svg>
                             </div>
                         </div>
-                        <div className='flex justify-between'>
+                        <div className='flex items-start justify-between'>
                             <h2 className='max-w-[193px] text-sm md:text-lg font-DanaMedium md:font-Dana line-clamp-2 text-zinc-700 dark:text-white'>طرز تهیه قهوه دمی با دستگاه اروپرس</h2>
-                            <div className='flex flex-col text-left text-emerald-600 dark:text-emerald-500 text-sm ml-[18px]'>
-                                <span className='text-2xl font-DanaDemiBold'>21</span>
-                                <span className='text-sm'>مرداد</span>
-                                <span className='text-sm'>1404</span>
+                            <div className='flex items-center'>
+                                <div className='w-px h-[61px] bg-gray-100 dark:bg-white/10 ml-5'></div>
+                                <div className='flex flex-col text-left  text-emerald-600 dark:text-emerald-500 text-sm ml-[18px]'>
+                                    <span className='text-2xl font-DanaDemiBold'>21</span>
+                                    <span className='text-sm'>مرداد</span>
+                                    <span className='text-sm'>1404</span>
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
