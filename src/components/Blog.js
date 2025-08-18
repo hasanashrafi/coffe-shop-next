d7d475e0d3c64c3bafdc4c3ff3fcd1ba42beea7e
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-18 17:55:22 +0330

    Add blog post card layout with image and date details in Blog component for improved content presentation.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,7 +6,7 @@ function Blog() {
     return (
         <section className='mb-8 md:mb-28'>
             <div className='container max-w-7xl'>
-           {/* section head */}
+                {/* section head */}
                 <div className="flex items-end justify-between mb-5 md:mb-12">
                     <div className="font-MorabbaMedium ">
                         <h3 className="text-2xl md:text-5xl text-zinc-700 dark:text-white">
@@ -26,7 +27,33 @@ function Blog() {
                 </div>
                 {/* section content */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
-                    
+                    <div className='bg-white dark:bg-zinc-700 p-2.5 md:pb-2 rounded-2xl shadow-md'>
+                        <div className='relative mb-4 rounded-2xl rounded-bl-4xl overflow-hidden'>
+                            <Image
+                                width={100}
+                                height={100}
+                                src={'/images/blogs/blog-1.png'}
+                                alt='blog'
+                                className='w-full h-full object-cover '
+                                priority
+                                sizes='100vw'
+                                quality={100}
+                            />
+                            <div className='absolute inset-0  md:flex-center hidden  bg-gradient-to-r from-orange-200/80 to-orange-300/80 rounded-xl'>
+                                <svg className='w-[138px] h-[58px] text-amber-700'>
+                                    <use href='#logo-type' />
+                                </svg>
+                            </div>
+                        </div>
+                        <div className='flex justify-between'>
+                            <h2 className='max-w-[193px] text-sm md:text-lg font-DanaMedium md:font-Dana line-clamp-2 text-zinc-700 dark:text-white'>طرز تهیه قهوه دمی با دستگاه اروپرس</h2>
+                            <div className='flex flex-col text-left text-emerald-600 dark:text-emerald-500 text-sm ml-[18px]'>
+                                <span className='text-2xl font-DanaDemiBold'>21</span>
+                                <span className='text-sm'>مرداد</span>
+                                <span className='text-sm'>1404</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
