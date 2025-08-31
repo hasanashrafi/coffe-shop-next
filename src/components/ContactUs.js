bf81ce96e0d572ea18b30d899dfb93070a266cf6
Hasan Ashrafi <yasashrafi@gmail.com>
2025-08-31 19:51:33 +0330

    Refactor ContactUs component layout and styling for improved responsiveness

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,7 +5,7 @@ import React from 'react'
 function ContactUs() {
     return (
         <section className='mb-16 md:mb-28'>
-            <div className='container mx-auto'>
+            <div className='container mx-auto max-w-7xl'>
                 <div className='flex flex-col lg:flex-row items-center lg:items-start gap-y-8 lg:gap-x-5 '>
                     <Image
                         width={296}
@@ -27,11 +27,11 @@ function ContactUs() {
                         <span></span>
                         <span></span>
                     </div>
-                    <p className='text-lg md:text-2xl'>
+                    <p className='tex-base md:text-xl'>
                         فضای گرم و دنج ما را احساس کنید، جایی که همه می توانند قهوه معطری پیدا کنند و دسرهای خوشمزه ما را که کاملاً با قهوه داغ همراه شده است، امتحان کنند. فضای داخلی شیک و کارکنان خوش برخورد ما روز شما را می سازد!
                     </p>
                     <Link href="/" 
-                    className='flex items-center gap-x-1 text-xs'>
+                    className='inline-flex items-center justify-center gap-x-2 px-6 mt-5 md:mt-6 h-[50px] md:h-[60px] border md:border-2 border-orange-300 rounded-full text-xl tracking-tightest text-orange-300 '>
                     <svg className='size-5 md:size-8'>
                         <use href='#phoneOut' />
                     </svg>
